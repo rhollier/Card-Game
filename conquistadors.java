4235f66df19be0cd1f3ccd30bd96f30aac623726
marypinedo99 <38365703+marypinedo99@users.noreply.github.com>
2018-10-23 11:20:02 -0500

    Update conquistadors.java

--- conquistadors.java
+++ conquistadors.java
@@ -3,43 +3,18 @@
 //Created on PC
 
 public class conquistadors{
+  
   public static void main(String[] args) {
-    //int cards_deck = 52;
-    Cards();
-
+    System.out.println("How many players to you want to simulate?");
+    int numberofusers = keyboard.nextInt();
+      
+      
+       
   }
 
 public static Cards(){
-int cards[] = 1:13;
-}//the card game did not have any royal cards
+
+  final static String[] ranks = {"2", "3","4","5","6","7","8", "9","10", "Jack", "Queen", "King", "Ace"};
 
 //distribute cards to players
 Card[] playerOne = new Card[]
-
-
-
-
-
-//for loop rounds = # players + 1
-
-    public class Card {
-
-        private int cardNum;
-        final static String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
-        final static String[] ranks = {"2", "3","4","5","6","7","8", "9","10", "Jack", "Queen", "King", "Ace"};
-
-        Card (int theCard) {
-            setCardNum (theCard);
-        }
-
-        public void setCardNum (int theCard) {
-            cardNum = (theCard >= 0 && theCard <= 51)? theCard: 0;
-        }
-
-        public int getCardNum() {
-            return cardNum;
-        }
-
-
-        System.out.println("How many players to you want to simulate?");
-*/
